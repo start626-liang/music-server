9c96f9244e02f8615a2ce99ec73e1172cc775e1d
liang <402284347@qq.com>
2019-02-22 00:40:20 +0800

    异步读取文件夹更换成同步读取

--- app.js
+++ app.js
@@ -3,16 +3,12 @@ const app = express()
 const fs = require("fs")
 
 app.use(express.static('public'))
-const musicList = []
-musicList.push(null)
-fs.readdir("public/CloudMusic",function(err, files){
-   if (err) {
-       return console.error(err);
-   }
-   files.forEach( function (file){
-        musicList.push(file)
-   });
-});
-app.get('/music/list', (req, res) => res.send(musicList))
+
+
+app.get('/music/list', (req, res) => {
+    const data = fs.readdirSync("public/CloudMusic");
+    data.unshift(null)
+    res.send(data)
+})
 
 app.listen(5000, () => console.log('Example app listening on port 5000!'))
